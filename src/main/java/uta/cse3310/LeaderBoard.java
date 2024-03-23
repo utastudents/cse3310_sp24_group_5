bfd3e2be9d49f18253175a2d11239241e0e153f4
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 21:17:12 -0500

    Update and rename Statistics.java to LeaderBoard.java

---
+++ src/main/java/uta/cse3310/LeaderBoard.java
@@ -0,0 +1,33 @@
+package uta.cse3310;
+
+import java.util.Map;
+import java.util.List;
+
+public class LeaderBoard 
+{
+    private Map<Player, Integer> scores;
+
+    //Constructor
+    public LeaderBoard() 
+    {
+       
+    }
+
+    // Method to add player score to leaderboard
+    public void addScore(Player player, int score) 
+    {
+        
+    }
+
+    //method to get top players for leaderboard
+    public List<Player> getTopPlayers(int count) 
+    {
+        return null;
+    }
+
+    //method to display leaderboard
+    public void display() 
+    {
+        
+    }
+}
