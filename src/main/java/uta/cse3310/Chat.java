33658d82a267708a72e10cf0a98442df0e4143e6
ThiML10 <157675695+ThiML10@users.noreply.github.com>
2024-03-23 01:43:32 +0000

    chat

---
+++ src/main/java/uta/cse3310/Chat.java
@@ -0,0 +1,12 @@
+public class Chat {
+    private String message;
+
+    // to send message
+    public void sendMessage(String message) {
+        
+    }
+
+    // to receive message
+    public void receiveMessage(String message) {
+    }
+}
