c3e60563c9d8d9e912ae84528c1c34af533b3494
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 20:57:38 -0500

    Updated formatting Timer.java

--- src/main/java/uta/cse3310/Timer.java
+++ src/main/java/uta/cse3310/Timer.java
@@ -1,22 +1,33 @@
-public class Timer {
+public class Timer 
+{
     private int timeRemaining;
     private boolean isRunning;
     private boolean isStopped;
 
 
-    public void startTimer(int initialTime) {
+    public void startTimer(int initialTime) 
+    {
+        
     }
 
-    public void resetTimer() {
+    public void resetTimer() 
+    {
+        
     }
 
-    public int getTimeRemaining() {
+    public int getTimeRemaining() 
+    {
+        
     }
 
-    public boolean isRunning() {
+    public boolean isRunning() 
+    {
+        
     }
 
-    public boolean isStopped() {
+    public boolean isStopped() 
+    {
+        
     }
 }
 
