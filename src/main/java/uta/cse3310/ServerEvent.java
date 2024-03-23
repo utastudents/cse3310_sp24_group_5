94f68d23e7b94ef28c27f2ba85f7371a2576c5c6
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 19:43:50 -0500

    Update ServerEvent.java

--- src/main/java/uta/cse3310/ServerEvent.java
+++ src/main/java/uta/cse3310/ServerEvent.java
@@ -4,18 +4,21 @@ public class ServerEvent
 {
     private String type;
     private Object data;
-    
+
+    //constructor
     public ServerEvent(String type, Object data) 
     {
        this.type = type;
        this.data = data;
     }
 
+    //method getType that returns a string value
     public String getType() 
     {
        return type;
     }
 
+    //method getData that returns an object value
     public Object getData() 
     {
        return data;
