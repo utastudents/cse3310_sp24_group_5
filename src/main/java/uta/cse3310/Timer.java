15fd84a9ca278ffce63e7cf8de021b4184b7e019
ThiML10 <157675695+ThiML10@users.noreply.github.com>
2024-03-23 01:29:51 +0000

    timer

---
+++ src/main/java/uta/cse3310/Timer.java
@@ -0,0 +1,24 @@
+public class Timer {
+    private int timeRemaining;
+    private boolean isRunning;
+    private boolean isStopped;
+
+
+    public void startTimer(int initialTime) {
+    }
+
+    public void resetTimer() {
+    }
+
+    public int getTimeRemaining() {
+    }
+
+    public boolean isRunning() {
+    }
+
+    public boolean isStopped() {
+    }
+}
+
+
+
