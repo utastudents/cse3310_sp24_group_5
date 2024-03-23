b27823e632a8e570807d963ee899ef0c5d9abbbd
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 19:30:32 -0500

    Update and rename HttpServer.java to HTTPServer.java

---
+++ src/main/java/uta/cse3310/HTTPServer.java
@@ -0,0 +1,32 @@
+package uta.cse3310;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import net.freeutils.httpserver.HTTPServer;
+import net.freeutils.httpserver.HTTPServer.ContextHandler;
+import net.freeutils.httpserver.HTTPServer.FileContextHandler;
+import net.freeutils.httpserver.HTTPServer.Request;
+import net.freeutils.httpserver.HTTPServer.Response;
+import net.freeutils.httpserver.HTTPServer.VirtualHost;
+
+// http server include is a GPL licensed package from
+//            http://www.freeutils.net/source/jlhttp/
+
+public class HTTPServer
+{
+   private String HTML;
+   private int port;
+
+   //contructor
+   public HTTPServer(int portNum) 
+   {  
+      this.port = portNum;
+   }
+
+   public void begin() 
+   {
+      
+   }
+}
