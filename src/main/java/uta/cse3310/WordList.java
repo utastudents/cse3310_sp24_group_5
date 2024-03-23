fe796f323fb9973776ccd80f6a46b7f15f393eb7
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 21:25:16 -0500

    Update and rename Board.java to WordList.java

---
+++ src/main/java/uta/cse3310/WordList.java
@@ -0,0 +1,26 @@
+package uta.cse3310;
+
+import java.util.List;
+
+public class WordList 
+{
+    private Word[] words;
+
+    //method for accesing the word list from file
+    public List<String> getWordList(String wordsFile) 
+    {
+        return null;
+    }
+
+    //method to update word list
+    public void updateWordList(Grid grid) 
+    {
+        
+    }
+
+    //method for word shuffling
+    public void shuffleWords() 
+    {
+      
+    }
+}
