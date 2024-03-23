4f807f30a28e9cf9db523d6185909c7df31e7f82
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 20:18:44 -0500

    Create Lobby.java

---
+++ src/main/java/uta/cse3310/Lobby.java
@@ -0,0 +1,33 @@
+import java.util.Vector;
+import java.util.HashMap;
+
+public class Lobby 
+{
+    private Vector<Player> players;
+    private Vector<Player> standby;
+    private HashMap<String, Game> games;
+
+    //contructor
+    public Lobby() 
+    {
+       
+    }
+
+    //method to chech nickname
+    public boolean checkNick(String nick) 
+    {
+        return false;
+    }
+
+    //method to add player to a game
+    public void addPlayerToGame(String gameId) 
+    {
+       
+    }
+
+    // method for a player to leave
+    public void leave(Player player) 
+    {
+       
+    }
+}
