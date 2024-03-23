d3e96593f171e853ae2d3340ce6a994259cc5ee0
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 19:40:29 -0500

    Update ServerEvent.java

--- src/main/java/uta/cse3310/ServerEvent.java
+++ src/main/java/uta/cse3310/ServerEvent.java
@@ -1,7 +1,23 @@
 package uta.cse3310;
 
-public class ServerEvent {
-    PlayerType YouAre; // Either an XPLAYER or a YPLAYER
-    int GameId;
+public class ServerEvent 
+{
+    private String type;
+    private Object data;
     
+    public ServerEvent(String type, Object data) 
+    {
+       this.type = type;
+       this.data = data;
+    }
+
+    public String getType() 
+    {
+       return type;
+    }
+
+    public Object getData() 
+    {
+       return data;
+    }
 }
