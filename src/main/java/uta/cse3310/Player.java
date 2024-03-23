9f56cc2f374137f6205ce216141bb5a7a32c2cb0
manuelh20013 <112411017+manuelh20013@users.noreply.github.com>
2024-03-22 20:35:20 -0500

    Update and rename PlayerType.java to Player.java

---
+++ src/main/java/uta/cse3310/Player.java
@@ -0,0 +1,54 @@
+package uta.cse3310;
+
+public class Player 
+{
+    private String nick;
+    private int score;
+    private int status;
+    private int totalPoints;
+    private int gameWin;
+
+    //Method for player nickname
+    public String getNick() 
+    {
+        return nick;
+    }
+
+    // Method for player score
+    public void setScore(int score) 
+    {
+        
+    }
+
+    // Method for player to select word in game
+    public void selectWord() 
+    {
+
+    }
+
+    // Method to return player status
+    public int getStatus() 
+    {
+        return status;
+    }
+
+    // Method to return player score for use elsewhere
+    public int getScore() 
+    {
+        return score;
+    }
+
+    // Method used to award a game win to the winning player
+    public int getGameWin() 
+    {
+        return gameWin;
+    }
+
+    // Method to increase player score
+    public void increaseScore(int points) 
+    {
+        
+    }
+}
+
+
