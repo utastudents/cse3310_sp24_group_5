97a3c85939aa72a3be00a70207e7e309791ccad5
muktar1907 <muktar1907@gmail.com>
2024-04-10 00:07:03 -0500

    Filling out some methods

--- src/main/java/uta/cse3310/Game.java
+++ src/main/java/uta/cse3310/Game.java
@@ -1,37 +1,57 @@
 package uta.cse3310;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Game //implements Chat, Timer, GridField, LeaderBoard, Wordlist
 {
     private int gameMode;
     private String gameID;
-    private Player[] playersList;
+    private ArrayList<Player>playersList = new ArrayList<Player>();
     private boolean gameStatus;
     private String wordsDone;
     private Timer timer;
     private int gameCount;
+    private GridField gridField;
 
+     
      // Method to determine gamemode
     public int getGameMode() 
     {
         return gameMode;
     }
-
-     // Method to get players game ID
+    //Method to set the gameMode
+    public void setGameMode(int gameMode)
+    {
+        this.gameMode=gameMode;
+    }
+    // Method to get players game ID
     public String getGameID() 
     {
         return gameID;
     }
-
-     // Method to start game
-    public void startGame() 
+    //Method to set game ID
+    public void setGameID(String gameID)
     {
-        
+        this.gameID=gameID;
+    }
+    // Method to get players game grid
+    public GridField getGridField() 
+    {
+        return gridField;
+    }
+    
+    // Method to start game
+    public boolean startGame() 
+    {
+        displayRules();
+        gridField.generateGrid();
+        return true;
     }
 
      // Method for adding players to the game screen
-    public Player[] addPlayers() 
+    public void addPlayers(Player newPlayer) 
     {
-        return null;
+        playersList.add(newPlayer);
     }
 
      // Method to update game state
@@ -41,13 +61,37 @@ public class Game //implements Chat, Timer, GridField, LeaderBoard, Wordlist
     }
 
      // Method for players to exit game
-    public void exitGame() 
+    public void exitGame(ArrayList<Player> playersList) 
     {
-        
+        //declare winner
+        int highestScore=0;
+        Player winner;
+        while(playersList.hasNext())
+        {
+            Player currPlayer=playersList.next();
+            if(currPlayer.score>highestScore)
+            {
+                highestScore=currPlayer.score;
+                winner=currPlayer;
+            }
+        }
+        System.out.pritntln(winner.nick +" is the winner")
+        System.out.println(winner.nick+" scored "winner.score+" points")
+        while(playersList.hasNext())
+        {
+            Player currPlayer=playersList.next();
+            if(currPlayer!=winner)
+            {
+                System.out.println(currPlayer.nick+" scored "currPlayer.score+" points")
+            }
+            
+        }
+       //remove players
+       playersList.clear();
     }
 
      // Method for chat implementation in game
-    public void chat(String message) 
+    public void chat(Player sender,String message) 
     {
        
     }
@@ -55,7 +99,8 @@ public class Game //implements Chat, Timer, GridField, LeaderBoard, Wordlist
     // Method to check for a valid word
     public boolean validWord(String word) 
     {
-        return false;
+        
+        return gridField.checkWord(word);
     }
 
     // Method for handling chosen word
@@ -67,7 +112,10 @@ public class Game //implements Chat, Timer, GridField, LeaderBoard, Wordlist
     // Method to update the game timer"Shot Clock"
     public void updateTimer() 
     {
-        
+        while(timer.isRunning())
+        {
+            System.out.println(timer.getTimeRemaining());
+        }
     }
 
     // Method to display rules of game
---
+++ src/test/java/uta/cse3310/gameTest.java
@@ -0,0 +1,22 @@
+package uta.cse3310;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class gameTest extends TestCase
+{
+    public gameTest(String test)
+    {
+        super(test);
+    }
+
+    public static Test suite() 
+    {
+        return new TestSuite(gameTest.class);
+    }
+
+    //The following are the unit tests for the game class
+    //-----------------------------------------------------------
+    //public void test
+}
