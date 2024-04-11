6d431c26ed8993174e037e4e21906aa9953b562b
muktar1907 <muktar1907@gmail.com>
2024-04-11 10:06:53 -0500

    Finish userEvent class

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -3,8 +3,13 @@ package uta.cse3310;
 
 public class UserEvent {
     int gameId;
-    Player player;
-    int x;
-    int y;
-    
+    Player player;//the player that made the event
+    int cell;//refers to the cell containing a selected letter
+    /*  in the script section of index.html there will
+        a function called select word that uses 3 other
+        functions:buttonClick,buttonhover,and button release
+        These 3 functions when executed one after the other
+        in order will create the selecting functionality
+
+    */
 }
