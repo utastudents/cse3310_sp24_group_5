47eae1d29a4beacb33b6f2da72d2ce7986639ab6
2onefan2 <159247397+2onefan2@users.noreply.github.com>
2024-04-12 01:21:42 -0500

    Updated GridField class to meet requirements

--- src/main/java/uta/cse3310/GridField.java
+++ src/main/java/uta/cse3310/GridField.java
@@ -2,41 +2,37 @@ package uta.cse3310;
 
 import java.util.ArrayList;
 
-public class GridField //extends WordList
-{
+public class GridField {
+
     private ArrayList<String> word;
     private int wordLength;
     private int remainingWords;
     private int gridSize;
     private char[][] grid;
 
-    public void generateGrid() 
-    {
-        
+    public void generateGrid() {
+        // Logic for generating the grid
     }
 
-    public boolean checkWord(String word) 
-    {
+    public boolean checkWord(String word) {
+        // Logic for checking if the word is valid
         return true;
     }
 
-    public int getRemainingWords() 
-    {
+    public int getRemainingWords() {
+        // Logic for getting the remaining words
         return 0;
     }
 
-    public void revealWord(String words) 
-    {
-        
+    public void revealWord(String words) {
+        // Logic for revealing a word
     }
 
-    public void addWord(String word, Direction direction) 
-    {
-        
+    public void addWord(String word, Direction direction) {
+        // Logic for adding a word to the grid
     }
 
-    public void displayGrid() 
-    {
-        
+    public void displayGrid() {
+        // Logic for displaying the grid
     }
 }
