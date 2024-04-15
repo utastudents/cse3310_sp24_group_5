f5d7b7a476b6e08b224ee0f71c7bb0bb1f69b79b
2onefan2 <159247397+2onefan2@users.noreply.github.com>
2024-04-15 18:25:04 -0500

    The DirectionTest class was modified to resolve a testing error caused by the absence of the Direction class or enum

--- src/test/java/uta/cse3310/DirectionTest.java
+++ src/test/java/uta/cse3310/DirectionTest.java
@@ -1,16 +1,24 @@
 package uta.cse3310;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class DirectionTest {
 
+    private Direction direction;
+
+    @Before
+    public void setUp() {
+        this.direction = new Direction();
+    }
+
     @Test
     public void testDirectionEnum() {
-        assertEquals(Direction.HORIZONTAL, Direction.valueOf("HORIZONTAL"));
-        assertEquals(Direction.VERTICAL, Direction.valueOf("VERTICAL"));
-        assertEquals(Direction.DIAGONAL, Direction.valueOf("DIAGONAL"));
+        assertEquals(Direction.Directions.HORIZONTAL, Direction.Directions.valueOf("HORIZONTAL"));
+        assertEquals(Direction.Directions.VERTICAL, Direction.Directions.valueOf("VERTICAL"));
+        assertEquals(Direction.Directions.DIAGONAL, Direction.Directions.valueOf("DIAGONAL"));
     }
 
     // Add more test methods as needed
